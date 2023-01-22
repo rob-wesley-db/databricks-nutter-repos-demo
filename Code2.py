d0540c0d7746a7694834c0b4f396484acb636bee
rob-wesley-db <107556866+rob-wesley-db@users.noreply.github.com>
2023-01-22 15:14:44 -0500

    Update Code2.py

--- Code2.py
+++ Code2.py
@@ -1,8 +1,6 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
 
-# added a comment
-# another comment
 def generate_data2(table_name="my_data"):
   df = SparkSession.getActiveSession().range(0,10)
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
